62caeeec64167352c3f338259afb7bbfbb0ed22a
Matteo Collina <matteo.collina@gmail.com>
2014-05-05 16:14:16 +0200

    Added the ability to fix a pattern for commands.

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 var Writable = require('readable-stream').Writable
   , inherits = require('inherits')
 
-function ActStream(seneca) {
+function ActStream(seneca, fixed) {
   if (!(this instanceof ActStream)) {
-    return new ActStream(seneca)
+    return new ActStream(seneca, fixed)
   }
 
   this.seneca = seneca
+  this._fixed = fixed
 
   Writable.call(this, {
     objectMode: true,
@@ -18,6 +19,10 @@ function ActStream(seneca) {
 inherits(ActStream, Writable)
 
 ActStream.prototype._write = function write(obj, skip, callback) {
+  for (var key in this._fixed) {
+    obj[key] = this._fixed[key]
+  }
+
   this.seneca.act(obj, callback)
 }
 
--- test.js
+++ test.js
@@ -3,13 +3,35 @@ var test      = require('tap').test
   , seneca    = require('seneca')
   , actStream = require('./')
 
-test('call a seneca service', function(t) {
+test('calling a seneca service', function(t) {
   t.plan(1)
 
   var s = seneca()
     , stream  = actStream(s)
     , pattern = { sample: 'call' }
     , message = { sample: 'call', hello: 'world' }
+
+  function check(arrived, done) {
+    delete arrived.actid$
+    t.deepEqual(arrived, message)
+    done(null)
+  }
+
+  s.add(pattern, check);
+
+  stream.end(message);
+})
+
+test('calling a seneca service with a default pattern', function(t) {
+  t.plan(1)
+
+  var s = seneca()
+    , pattern = { sample: 'call' }
+    , stream  = actStream(s, pattern)
+    , message = { hello: 'world' }
+    , expectedMessage = {
+          sample: 'call'
+        , hello: 'world' }
 
   function check(arrived, done) {
     delete arrived.actid$
